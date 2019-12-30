4b159a3f1d023e493931077934554afffe681843
Lee Jae Hun <wogns5206@naver.com>
2019-12-30 19:51:39 +0900

    Node.js 예제따라하기 chapter3

--- chapter3/chapter3-8/error3.js
+++ chapter3/chapter3-8/error3.js
@@ -11,3 +11,7 @@ setInterval(() => {
 setTimeout(() => {
     console.log('실행됩니다');
 }, 2000);
+
+// 노드 공식 문서에서는 uncaughtException 이벤트를 최후의 수단으로 사용하라고 되어있씁니다.
+// 노드는 uncaughtException 이벤트 발생 후 다음 동작이 제대로 동작하는지를 보증하지 않습니다.
+// 따라서 단순히 에러 내용을 기록하는 정도로 사용하고 process.exit()로 프로세스를 종료하는 것이 좋습니다.
